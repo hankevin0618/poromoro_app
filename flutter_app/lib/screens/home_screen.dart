e34d4e5b91e7bcb80af7369ba14587df1d92edf3
kevin-han <kevin.han@autogeneral.com.au>
2023-02-02 20:11:45 +1000

    App bar

--- flutter_app/lib/screens/home_screen.dart
+++ flutter_app/lib/screens/home_screen.dart
@@ -1,129 +1,16 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  static const initialAmount = 1500;
-  int totalSeconds = initialAmount;
-  bool isRunning = false;
-  int totalPomodoros = 0;
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    if(totalSeconds == 0){
-      setState(() {
-        totalPomodoros++;
-        isRunning = false;
-        totalSeconds = initialAmount;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds--;
-      });
-    }
-  }
-
-  void onStartPressed() {
-    isRunning = true;
-    timer = Timer.periodic(const Duration(seconds: 1), onTick );
-  }
-
-  void onPausePressed() {
-    setState(() {
-      isRunning = false;
-      timer.cancel();
-    });
-  }
-
-  String format(int seconds){
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split(".").first.substring(2, 7);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 3,
-            child: Container(
-              child: Center(
-                child: IconButton(
-                  iconSize: 98,
-                  icon: Icon(
-                    isRunning 
-                      ? Icons.pause_circle
-                      : Icons.play_circle,
-                    color: Theme.of(context).cardColor,
-                  ),
-                  onPressed: isRunning ? onPausePressed : onStartPressed,
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Pomodoros",
-                          style: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.displayLarge?.color,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "$totalPomodoros",
-                          style: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.displayLarge?.color,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 40,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        elevation: 1,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: const Text("Today's Toons", style: TextStyle(fontSize: 26),),
       ),
     );
   }
