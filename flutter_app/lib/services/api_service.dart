84737b1af0cbad997aeddb6b36c4e483b5327d9b
kevin-han <kevin.han@autogeneral.com.au>
2023-02-02 20:33:54 +1000

    pass to hankevin0618

---
+++ flutter_app/lib/services/api_service.dart
@@ -0,0 +1,19 @@
+
+import 'package:http/http.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  void getTodaysToons() async {
+    // install http in pub.dev
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await get(url);
+    // if(response.statusCode == 200) {
+    //   print(response.body);
+    //   return;
+    // }
+    // throw Error();
+  }
+
+}
